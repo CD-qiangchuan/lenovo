4992f5c92c0975b822da4736bfae28e97d1faa2b
annLty <282400210@qq.com>
2014-08-20 17:19:10 +0800

    特惠专区完成
    首页、特惠专区、产品展示、产品详情页均完成2014/8/20

--- js/global.js
+++ js/global.js
@@ -1,7 +1,9 @@
 $(function(){
 	//首页悬浮
 	showScroll();
-	//shopping-cart's hover js
+	//首页左右切换产品
+	abc();
+	//首页shopping-cart's hover js
 	$(".hd-tool-shopping").hover(function(){
 		$(this).removeClass("shopping-sty").addClass("shopping-hover");
 		$(".hd-shopping-ico").removeClass("shopping-ico-bg").addClass("shopping-ico-hover");
@@ -13,7 +15,7 @@ $(function(){
 			$(".hd-tool-jt").removeClass("tool-jt-hover").addClass("tool-jt-bg");
 			$(".shopping-minicart").hide();
 			});
-	//sidebar's hover js
+	//首页sidebar's hover js
 	$(".sidebar-ul>#sidebar-li").hover(function(){
 		$(this).children("div.sub-sidebar").show();
 		$(this).addClass("sidebar-hover").removeClass("sidebar-f-h");
@@ -21,34 +23,36 @@ $(function(){
 			$(this).children("div.sub-sidebar").hide();
 			$(this).addClass("sidebar-f-h").removeClass("sidebar-hover");
 	});
-	//推荐专区
+	//首页推荐专区
 	$(".nominate-ul>li").hover(function(){
 		$(this).addClass("nominate-hover").removeClass("nominate-h");
 		},function(){
 			$(this).addClass("nominate-h").removeClass("nominate-hover");
 			});
-	//特惠专区鼠标悬停图片向左移动
+	//首页特惠专区鼠标悬停图片向左移动
 	$(".recommend-prc-img a").hover(function() {
         $("img",this).stop().animate({"margin-left":"-20px"},{queue:false,duration:200});
     },function(){
 		$("img",this).stop().animate({"margin-left":"0px"},{queue:false,duration:200});
 		});
-	//排行榜鼠标悬停图片向左移动
+	//首页排行榜鼠标悬停图片向左移动
 	$(".tab-hot-img a").hover(function(){
 		$("img",this).stop().animate({"margin-left":"-20px"},{queue:false,duration:200});
     },function(){
 		$("img",this).stop().animate({"margin-left":"0px"},{queue:false,duration:200});
 		});	
 	//首页右边悬浮层
-	$(".right-sidebar-item>a").hover(function(){
-		$(this).find("span").removeClass("sidebar-hid").addClass("sidebar-active");
+	$(".right-sidebar-item>li").hover(function(){
+		$(this).find("span").addClass("span-hidden");
+		$(this).find("em").addClass("sidebar-active");
 		},function(){
-		$(this).find("span").removeClass("sidebar-active").addClass("sidebar-hid");
-			});
+			$(this).find("span").removeClass("span-hidden");
+		$(this).find("em").removeClass("sidebar-active");
+			})
 	$(".right-sidebar-item>a").click(function(){
-		$(this).find("span").removeClass("sidebar-hid").addClass("sidebar-active");
+		$(this).find("em").removeClass("sidebar-hid").addClass("sidebar-active");
 		},function(){
-		$(this).find("span").removeClass("sidebar-active").addClass("sidebar-hid");
+		$(this).find("em").removeClass("sidebar-active").addClass("sidebar-hid");
 			});
 	//首页左边悬浮层
 	   //订单
@@ -89,6 +93,56 @@ $(function(){
 		$(this).addClass("left-sidebar-contact").removeClass("left-sidebar-contactH");
 		$(this).find(".left-contact-box").removeClass("left-side-active");
 			});
+	//首页当浏览器的宽度小于1366px时，悬浮层距离浏览器边框的距离为0
+	var width=$(window).width();
+	if(width<=1280){
+		 var ul=document.getElementsByClassName("left-sidebar");
+		 alert(ul.length);
+		 $(ul).addClass("left-sidebar-on");
+		var ul2=document.getElementsByClassName("right-sidebar");
+		$(ul2).addClass("right-sidebar-on");
+		}
+	//特惠专区页面“所有分类”hover
+	$(".pd-classify-h3").hover(function(){
+		$(this).find(".down-ico").addClass("up-ico");	
+		$(this).children(".all-classify-list").addClass("classify-list-on");
+		},function(){
+			$(this).find(".down-ico").removeClass("up-ico");
+			$(this).children(".all-classify-list").removeClass("classify-list-on");
+			});
+			
+	//产品内容页面图片展示
+	$(".pd-good-imgItem>ul li").click(function(){
+		$('.pd-good-imgItem>ul>li').removeClass('pd-pic-active');
+		$('.pd-good-imgItem>ul>li>.trigon-bg').removeClass('trigon-show');
+		$(this).find(".trigon-bg").addClass("trigon-show");
+			$(this).addClass('pd-pic-active');
+		});
+	//产品内容页面配送地点
+	$("#pd-store-selector").hover(function(){
+		$(this).find(".pd-delivery-province").addClass("trigon-show");
+		},function(){
+			$(this).find(".pd-delivery-province").removeClass("trigon-show");
+			});
+			
+	//产品展示页面筛选板块
+	$(".ps-reset-sublist>li").click(function(){
+		$(".ps-reset-sublist>li").removeClass("reset-active");
+		$(".ps-reset-sublist>li>.ps-setItem-sublist").removeClass("trigon-show");
+		$(this).addClass("reset-active");
+		$(this).find("ul.ps-setItem-sublist").addClass("trigon-show");
+		});
+    //产品展示页面内容排行
+	$(".ps-goods-term>li").click(function(){
+		$(".ps-goods-term>li").removeClass("reset-active");
+		$(this).addClass("reset-active");
+		})
+	//特惠专区鼠标悬停图片向左移动
+	$(".pf-goods-img").hover(function(){
+		$("img",this).stop().animate({"margin-left":"-20px"},{queue:false,duration:200});
+    },function(){
+		$("img",this).stop().animate({"margin-left":"0px"},{queue:false,duration:200});
+		});	
 	});
 //首页公告滚动方法
 function AutoScroll(obj){
@@ -98,6 +152,19 @@ function AutoScroll(obj){
                 $(this).css({marginTop:"0px"}).find("li:first").appendTo(this);
         });
 };
+//首页左右切换特卖产品      
+   function abc(){var $obj = $('.recommend-prc-box>li');var len  = $obj.length;var i = 0; 
+		$(".recommend-per").click(function(){i++;if(i==len){ i = 0;}  
+              $obj.stop(true,true).hide().eq(i).fadeIn(600);return false;  
+         });      
+         $(".recommend-next").click(function(){  
+              i--;  
+              if(i==-1){  
+                i = len-1;  
+              }  
+              $obj.stop(true,true).hide().eq(i).fadeIn(600);  
+              return false;  
+         }); }    
 //首页通用的选项卡切换方法
 function setTab(x,y,w) {
 	var oDiv=document.getElementById(x);//要显示的UL
@@ -116,7 +183,7 @@ function setTab(x,y,w) {
 //首页左右悬浮板块
 function showScroll(){
 			$(window).scroll( function() { 
-				var scrollValue=document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+				var scrollValue=document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;//兼容不同浏览器的高度获取
 				scrollValue > 200 ? $('div[class=left-sidebar]').fadeIn():$('div[class=left-sidebar]').fadeOut();
 				scrollValue > 400 ? $('div[class=right-sidebar]').fadeIn():$('div[class=right-sidebar]').fadeOut();
 			} );	
@@ -125,6 +192,34 @@ function showScroll(){
 			});	
 					
 		}
+
+/*===========================点击展开关闭效果====================================
+oSourceObj    当前控件
+oTargetObj    目标控件
+shutAble    是否可以关闭开关
+oOpenTip    展开时显示标题
+oShutTip    关闭时显示标题
+*/
+function openShutManager(oSourceObj,oTargetObj,shutAble,oOpenTip,oShutTip){
+var sourceObj = typeof oSourceObj == "string" ? document.getElementById(oSourceObj) : oSourceObj;
+var targetObj = typeof oTargetObj == "string" ? document.getElementById(oTargetObj) : oTargetObj;
+var openTip = oOpenTip || "";
+var shutTip = oShutTip || "";
+if(targetObj.style.display!="none"){
+   if(shutAble) return;
+   targetObj.style.display="none";
+    sourceObj.style.backgroundPosition="left -1680px";
+   if(openTip  &&  shutTip){
+    sourceObj.innerHTML = shutTip; 
+   }
+} else {
+   targetObj.style.display="block";
+   sourceObj.style.backgroundPosition="left -2085px";
+   if(openTip  &&  shutTip){
+    sourceObj.innerHTML = openTip; 
+   }
+}
+};
    
 
    
