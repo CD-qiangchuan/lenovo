2c8eff95bc5cd1eca0a5b90b2fa76f13a78ef372
li903986396 <903986396@qq.com>
2014-08-25 17:34:54 +0800

    解决jquery兼容问题
    修改首页CSS 列表页分页标签
    BY DIY

--- js/global.js
+++ js/global.js
@@ -243,6 +243,25 @@ if(targetObj.style.display!="none"){
 }
 };
 
-   
+/*IE 兼容getElementsByClassName*/
+
+function getElementsByClassName(className,tagName) { 
+    tagName=tagName||"*";                                    //参数tagName可以不写。
+    if (document.getElementsByClassName) {                    //如果浏览器支持getElementsByClassName就直接用
+        return getElementsByClassName(className);
+    }else { 
+        var tag= document.getElementsByTagName(tagName);    //获取指定元素
+        var tagAll = [];                                    //用于存储符合条件的元素
+        for (var i = 0; i < tag.length; i++) {                //遍历获得的元素
+            for(var j=0,n=tag[i].className.split(' ');j<n.length;j++){    //遍历此元素中所有class的值，如果包含指定的类名，就赋值给tagnameAll
+                if(n[j]==className){
+                    tagAll.push(tag[i]);
+                    break;
+                }
+            }
+        }
+        return tagAll;
+    }
+}
 
    
